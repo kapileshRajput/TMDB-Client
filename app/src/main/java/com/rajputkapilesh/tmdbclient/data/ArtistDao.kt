fccee281e7e580caa0f5735815088ee28a174696
Kapilesh Rajput <kapilesh@pop-os.localdomain>
2021-11-13 12:45:40 +0530

    Added Dao interfaces for each table

---
+++ app/src/main/java/com/rajputkapilesh/tmdbclient/data/ArtistDao.kt
@@ -0,0 +1,20 @@
+package com.rajputkapilesh.tmdbclient.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.rajputkapilesh.tmdbclient.data.model.artist.Artist
+
+@Dao
+interface ArtistDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveArtists(artists: List<Artist>)
+
+    @Query("DELETE FROM popular_artists")
+    suspend fun deleteAllArtists()
+
+    @Query("SELECT * FROM popular_artists")
+    suspend fun getArtists(artists: List<Artist>)
+}
---
+++ app/src/main/java/com/rajputkapilesh/tmdbclient/data/TvShowDao.kt
@@ -0,0 +1,20 @@
+package com.rajputkapilesh.tmdbclient.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.rajputkapilesh.tmdbclient.data.model.tvshow.TvShow
+
+@Dao
+interface TvShowDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveTvShows(tvShows: List<TvShow>)
+
+    @Query("DELETE FROM popular_tvShows")
+    suspend fun deleteAllTvShows()
+
+    @Query("SELECT * FROM popular_tvShows")
+    suspend fun getTvShows(tvShows: List<TvShow>)
+}
