365d546d35a68dfcb234c29219d4970da26c35d0
Kapilesh Rajput <kapilesh@pop-os.localdomain>
2021-11-13 12:20:48 +0530

    Added data classes

---
+++ app/src/main/java/com/rajputkapilesh/tmdbclient/data/ArtistList.kt
@@ -0,0 +1,15 @@
+package com.rajputkapilesh.tmdbclient.data
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ArtistList(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val artists: List<Artist>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
